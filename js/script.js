59ca8a4bdfb5668a8667646c26c79d3150fd0b66
viteshbava <82492234+viteshbava@users.noreply.github.com>
2023-06-16 18:27:14 +1200

    Initial holding site

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// COPY TO CLIPBOARD
+
+const copyToClipboardButtons = document.querySelectorAll('[data-copy-text]');
+
+for (const button of copyToClipboardButtons) {
+  button.addEventListener('click', () => {
+    // Create a new textarea element
+    const textarea = document.createElement('textarea');
+    // Set the value of the textarea to the text you want to copy
+    textarea.value = button.dataset.copyText;
+
+    // Append the textarea to the HTML document
+    document.body.appendChild(textarea);
+
+    // Select the text inside the textarea
+    textarea.select();
+
+    // Copy the selected text to the clipboard
+    document.execCommand('copy');
+
+    // Remove the textarea from the HTML document
+    document.body.removeChild(textarea);
+  });
+}
